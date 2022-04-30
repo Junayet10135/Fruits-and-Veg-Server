6aa0e0ad17a865182c7d48a48f3dabe7c006b0bd
JunayetTamim <junayettamim186@gmail.com>
2022-05-01 02:28:34 +0600

    connect with client site

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,13 +12,25 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b12rw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("fruitAndVeg").collection("inventory");
-    // perform actions on the collection object
-    console.log('mongo is connected');
-    client.close();
-});
 
+async function run(){
+    try{
+        await client.connect();
+        const inventoryCollection = client.db('fruitAndVeg').collection('inventory');
+
+        app.get('/inventory', async (req, res)=>{
+            const query = {};
+            const cursor = inventoryCollection.find(query);
+            const inventory =await cursor.toArray();
+            res.send(inventory);
+        })
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/',(req,res)=>{
     res.send('running')
